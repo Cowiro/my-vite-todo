b40b7d873ca8b3ae30cfe6b4fb89b410cc4323ec
Fuki <wisteria.f.120@gmail.com>
2022-12-12 21:00:41 +0900

    Refactor 練習用コードを削除して整理

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,6 @@
 <script setup>
 import TheHeader from './components/TheHeader.vue';
 import TheFooter from './components/TheFooter.vue';
-import { useRouter } from 'vue-router';
-
-const router = useRouter();
-const goBlog = () => {
-  router.push('/blog');
-};
 </script>
 
 <template>
@@ -15,9 +9,7 @@ const goBlog = () => {
     <nav>
       <router-link to="/">Home</router-link>
       | <router-link to="/about">About</router-link> |
-      <span @click="goBlog()">Blog</span>
-      | <router-link to="/blog/1">Blog1</router-link> |
-      <router-link to="/blog/2">Blog2</router-link>
+      <router-link to="/blog">Blog</router-link>
     </nav>
     <main class="main"><router-view /></main>
     <TheFooter />
